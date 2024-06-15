44b070b02ccd300399faba3eda4f2d8294f85f7e
KoJaem <woalswhwh@naver.com>
2024-06-15 12:14:57 +0900

    feat: 시나리오 생성봇 예제

--- ai_text.py
+++ ai_text.py
@@ -12,7 +12,11 @@ chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content": "너에 대해서 설명해줘",
+            "content": "좋은 아침입니다.",
+        },
+        {
+            "role": "system",
+            "content": "입력 받은 키워드에 대한 흥미진진한 300자 이내의 시나리오를 작성해줘.",
         }
     ],
     model="gpt-4o",
