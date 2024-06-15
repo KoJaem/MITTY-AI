8366fad1d7a5be78738d453ab89d21437adc6612
KoJaem <woalswhwh@naver.com>
2024-06-15 16:33:02 +0900

    feat: Modified chatbot logic

--- app.py
+++ app.py
@@ -9,34 +9,59 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-st.title('홍보 포스터 만들기 🚀')
+st.title("KoJaem's bot 🚀")
 
-keyword = st.text_input("키워드를 입력하세요")
 
-if st.button('생성하기'):
-  with st.spinner('생성중...'):
+type = st.radio(
+    "어떤 작업이 필요하세요?",
+    [":rainbow[이미지 생성]", "***Text 생성***", "필요없어요"],
+    captions = ["필요한 이미지를 생성해드립니다.", "요구사항에 맞게 텍스트를 출력해드립니다.", "😢"])
+
+keyword = st.text_input('필요한것을 입력해주세요.')
+
+if st.button('요청하기'):
+  if(type == ':rainbow[이미지 생성]'):
+    with st.spinner('이미지 생성중...'):
+        chat_completion = client.chat.completions.create(
+          messages=[
+              {
+                  "role": "user",
+                  "content": keyword,
+              },
+              {
+                  "role": "system",
+                  "content": "사용자가 말한 content 에 대해 다양한 정보를 알려줘.",
+              }
+          ],
+          model="gpt-4o",
+          )
+        response = client.images.generate(
+          model="dall-e-3",
+          prompt=f'{keyword}',
+          quality="standard",
+          size="1024x1024",
+          n=1,
+      )
+        result = chat_completion.choices[0].message.content
+        image_url = response.data[0].url
+        st.write(result)
+        st.image(image_url)
+  elif(type == '***Text 생성***'):
+    with st.spinner('텍스트 생성중...'):
       chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": keyword,
-            },
-            {
-                "role": "system",
-                "content": "입력 받은 키워드에 대한 150자 이내에 솔깃한 제품 홍보 문구를 작성해줘.",
-            }
-        ],
-        model="gpt-4o",
-        )
-      response = client.images.generate(
-        model="dall-e-3",
-        prompt=f'{keyword}를 수채화풍으로 그려줘',
-        quality="standard",
-        size="1024x1024",
-        n=1,
-    )
-
-  result = chat_completion.choices[0].message.content
-  image_url = response.data[0].url
-  st.write(result)
-  st.image(image_url)
+      messages=[
+          {
+              "role": "user",
+              "content": keyword,
+          },
+          {
+              "role": "system",
+              "content": "사용자가 말한 요구사항을 친절하게 300자 이내로 답변해줘",
+          }
+      ],
+      model="gpt-4o",
+      )
+      result = chat_completion.choices[0].message.content
+      st.write(result)
+  else:
+    st.write('필요없다면서 왜 누르세요 😢😢😢')
