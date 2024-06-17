1861f768b7a7599a11f071005a568a52c9c64bd2
KoJaem <woalswhwh@naver.com>
2024-06-17 21:16:42 +0900

    feat: Removed python code and NextJS initial settings

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-[4px]">
+      <p className="bg-primary-10 text-white">팔레트1</p>
+      <p className="bg-primary-20 text-white">팔레트2</p>
+      <p className="bg-primary-30 text-white">팔레트3</p>
+      <p className="bg-primary-40 text-white">팔레트4</p>
+      <p className="bg-primary-50 text-white">팔레트5</p>
+      <p className="bg-primary-60 text-white">팔레트6</p>
+      <p className="bg-primary-70 text-white">팔레트7</p>
+      <p className="bg-primary-80 text-white">팔레트8</p>
+      <p className="bg-primary-90 text-white">팔레트9</p>
+      <p className="bg-primary text-white">팔레트10</p>
+    </div>
+  );
+}
