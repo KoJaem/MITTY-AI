7023bb8f73623543dea8cbdb9a3765c2af8e703b
KoJaem <woalswhwh@naver.com>
2024-06-17 23:10:18 +0900

    feat: Implemented drawingCharacter

---
+++ src/pages/api/drawingCharacter.ts
@@ -0,0 +1,76 @@
+import axios from "axios";
+import { NextApiRequest, NextApiResponse } from "next";
+import OpenAI from "openai";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  const OPEN_API_KEY = process.env.OPENAI_API_KEY;
+
+  const openai = new OpenAI({
+    apiKey: OPEN_API_KEY,
+  });
+
+  const { image } = req.body;
+
+  if (!image) {
+    return res.status(400).json({ error: "No image provided" });
+  }
+
+  try {
+    const analytics = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: [
+        {
+          role: "user",
+          content: [
+            {
+              type: "image_url",
+              image_url: {
+                url: image,
+              },
+            },
+          ],
+        },
+        {
+          role: "system",
+          content:
+            "입력받은 이미지는 사용자의 얼굴이야. 얼굴에 대해서 최대한 분석해줘. 눈, 코, 입, 헤어스타일, 표정, 성별 등등 분석할 수 있는 내용을 모두 분석해. 잡다한 이야기는 하지말고, 분석한 특징들에 대해서만 최대한 많이 나열해서 답변해.",
+        },
+      ],
+    });
+
+    const userFaceInfo = analytics?.choices[0]?.message?.content;
+
+    // console.log("userFaceInfo", userFaceInfo);
+
+    const response = await openai.images.generate({
+      model: "dall-e-3",
+      prompt: `${userFaceInfo} 은 사용자의 얼굴에 대한 정보야. 이 정보를 기반으로 캐릭터를 하나 만들어줘.`,
+      n: 1,
+      size: "1024x1024",
+    });
+
+    // console.log("response.data[0].url", response.data[0].url);
+
+    res.status(200).json(response.data[0].url);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(
+        "Error communicating with OpenAI API:",
+        error.response?.data || error.message
+      );
+      res.status(500).json({
+        error: error.response?.data || "Error communicating with OpenAI API",
+      });
+    } else {
+      console.error("Unexpected error:", error);
+      res.status(500).json({ error: "Unexpected error occurred" });
+    }
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,66 @@
-export default function Home() {
+import axios from 'axios';
+import { useRef, useEffect } from "react";
+
+export default function VideoCapture() {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const initCamera = async() => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (err) {
+        console.error("Error accessing camera: ", err);
+      }
+    }
+    initCamera();
+  }, []);
+
+  const captureImage = () => {
+    const canvas = canvasRef.current;
+    const video = videoRef.current;
+    if (canvas && video) {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const context = canvas.getContext("2d");
+      if (context) {
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        const dataUrl = canvas.toDataURL("image/jpeg");
+        sendImageToAPI(dataUrl);
+      }
+    }
+  };
+
+  const sendImageToAPI = async (base64Image: string) => {
+    try {
+      const response = await axios.post(
+        "/api/drawingCharacter",
+        {
+          image: base64Image,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Image processed:", response.data);
+    } catch (error) {
+      console.error("Error uploading image", error);
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-[4px]">
-      <p className="bg-primary-10 text-white">팔레트1</p>
-      <p className="bg-primary-20 text-white">팔레트2</p>
-      <p className="bg-primary-30 text-white">팔레트3</p>
-      <p className="bg-primary-40 text-white">팔레트4</p>
-      <p className="bg-primary-50 text-white">팔레트5</p>
-      <p className="bg-primary-60 text-white">팔레트6</p>
-      <p className="bg-primary-70 text-white">팔레트7</p>
-      <p className="bg-primary-80 text-white">팔레트8</p>
-      <p className="bg-primary-90 text-white">팔레트9</p>
-      <p className="bg-primary text-white">팔레트10</p>
+    <div>
+      <video ref={videoRef} autoPlay playsInline></video>
+      <canvas ref={canvasRef} style={{ display: "none" }}></canvas>
+      <button onClick={captureImage}>Capture Image</button>
     </div>
   );
 }
